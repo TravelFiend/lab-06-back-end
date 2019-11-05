576c4c484fb75d448e5937f5543e1adaed76580a
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2019-11-05 11:09:32 -0800

    data result hope in the making

--- server.js
+++ server.js
@@ -1,12 +1,48 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const geoData = require('./data/geo.json');
+
 
 const PORT = process.env.PORT;
 
+app.use(cors());
+
 app.use(express.static('./public'));
 
+app.get('/location', (req, res) => {
+    try {
+        const location = req.query.location;
+        const result = getLatLng(location);
+        res.status(200).json(result);
+    }
+    catch (err){
+        res.status(500).send('Hey, it did not work. Try again.');
+    }
+});
+
+const getLatLng = (location) => {
+    if (location === 'bad location') {
+        throw new Error();
+    }
+
+    return toLocation(geoData);
+}; 
+
+const toLocation = () => {
+
+    const firstResult = geoData.results[0];
+    const geometry = firstResult.geometry;
+
+    return {
+        formatted_query : firstResult.formatted_address,
+        latitude : geometry.location.lat,
+        longitude : geometry.location.lng,
+    };
+};
+
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+    console.log('Listening on port', PORT);
 });
 

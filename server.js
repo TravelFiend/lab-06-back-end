9664926341606f88646d4859287cbbc406e1785a
TravelFiend <mikegrace274@gmail.com>
2019-11-06 09:12:32 -0800

    weather started

--- server.js
+++ server.js
@@ -1,15 +1,35 @@
 require('dotenv').config();
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const geoData = require('./data/geo.json');
+const weatherData = require('./data/darksky.json');
 
+const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 
 app.use(express.static('./public'));
+
+const toLocation = () => {
+    const firstResult = geoData.results[0];
+    const geometry = firstResult.geometry;
+    
+    return {
+        formatted_query : firstResult.formatted_address,
+        latitude : geometry.location.lat,
+        longitude : geometry.location.lng,
+    };
+};
+
+const getLatLng = (location) => {
+    if (location === 'bad location') {
+        throw new Error();
+    }
+    
+    return toLocation(geoData);
+}; 
 
 app.get('/location', (req, res) => {
     try {
@@ -22,24 +42,26 @@ app.get('/location', (req, res) => {
     }
 });
 
-const getLatLng = (location) => {
-    if (location === 'bad location') {
+app.get('/weather', (req, res) => {
+    try {
+        const weather = req.query.weather;
+        const result = getWeather(weather);
+        res.status(200).json(result);
+    }
+    catch (err) {
+        res.status(500).send('Error, try again!');
+    }
+});
+
+const getWeather = () => {
+    if (weather === 'bad weather'){
         throw new Error();
     }
+    return toWeather(weatherData);
+};
 
-    return toLocation(geoData);
-}; 
-
-const toLocation = () => {
-
-    const firstResult = geoData.results[0];
-    const geometry = firstResult.geometry;
-
-    return {
-        formatted_query : firstResult.formatted_address,
-        latitude : geometry.location.lat,
-        longitude : geometry.location.lng,
-    };
+const toWeather = () => {
+    const firstResult = weatherData.daily.data;
 };
 
 app.listen(PORT, () => {

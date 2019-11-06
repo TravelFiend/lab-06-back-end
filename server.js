c5d8988b6c2029fa32a8e265e03d2518f2cd5e9c
TravelFiend <mikegrace274@gmail.com>
2019-11-06 13:44:40 -0800

    cleaned up

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-// const geoData = require('./data/geo.json');
-//const weatherData = require('./data/darksky.json');
 const superagent = require('superagent');
 
 const app = express();
@@ -15,9 +13,6 @@ app.use(express.static('./public'));
 let latAndLng;
 
 const toLocation = placeItem => {
-    // const latitude = placeItem.geometry.location.lat; 
-    // const longitude = placeItem.geometry.location.lng;
-    // const formatQ = placeItem.formatted_address;
     const {
         geometry: {
             location: {
@@ -36,14 +31,6 @@ const toLocation = placeItem => {
     };
 };
 
-// const getLatLng = (location) => {
-//     if (location === 'bad location') {
-//         throw new Error();
-//     }
-    
-//     return toLocation(geoData);
-// }; 
-
 app.get('/location', async(req, res) => {
 
     const mapsApiKey = process.env.GOOGLE_MAPS_API_KEY;
@@ -56,36 +43,11 @@ app.get('/location', async(req, res) => {
     res.json(response);
 });
 
-// const toWeather = () => {
-//     const firstResult = weatherData.daily.data[0];
-
-//     return {
-//         forecast: firstResult.summary,
-//         time: new Date(firstResult.time * 1000).toDateString()
-//     };
-// };
-
-// app.get('/weather', (req, res) => {
-//     try {
-//         const weather = req.query.weather;
-//         const result = toWeather(weather);
-//         res.status(200).json([result]);
-//     }
-//     catch (err) {
-//         res.status(500).send('Error, try again!');
-//     }
-// });
-
-
 const getWeather = async(lat, lng) => { 
-    //lat = latAndLng.lat;
-    //lng = latAndLng.lng;
     const darkskyApikey = process.env.DARKSKY_KEY;
     const actualWeather = await superagent.get(`https://api.darksky.net/forecast/${darkskyApikey}/${lat},${lng}`);
     const parsedWeather = JSON.parse(actualWeather.text);
-    //const result = {
-        //forcast : parsedWeather.daily.data.summary,
-        //time: new Date(
+
     const newWeatherArray = (parsedWeather.daily.data).map(weatherItem => {
         return {
             forecast : weatherItem.summary,
